87ef740eaedd0a091f6b26c82893f81d070d1fe4
rahat saini <rahatsaini@yahoo.com>
2019-09-01 14:16:35 +1000

    Firefox and chrome browser changes

--- CucumberAssessmentProject_JUNIT/src/main/java/utils/SeleniumDriver.java
+++ CucumberAssessmentProject_JUNIT/src/main/java/utils/SeleniumDriver.java
@@ -4,52 +4,65 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumDriver {
-	
+
 	private static SeleniumDriver seleniumDriver;
-	
+
 	//init WebDriver
 	private static WebDriver driver;
-	
+
+	String browserSpecified;
 	//waits
 	private static WebDriverWait waitDriver;
 	public final static int TIMEOUT = 40;
 	public final static int PAGE_LOAD_TIMEOUT = 50;
-	
-	private SeleniumDriver() {	
-		
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\chromedriver.exe");
-		driver = new ChromeDriver();
+
+	private SeleniumDriver(String givenBrowser) {	
+
+		browserSpecified = givenBrowser;
+
+		if (browserSpecified.equalsIgnoreCase("chrome")) {
+
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+
+		if (browserSpecified.equalsIgnoreCase("firefox")) {
+
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
 		driver.manage().window().maximize();
-		
+
 		waitDriver = new WebDriverWait(driver,TIMEOUT);
 		driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(TIMEOUT, TimeUnit.SECONDS);
-		
+
 	}
-	
+
 	public static void launchPage(String url) {
-		
+
 		driver.get(url);
 	}
-	
+
 	public static WebDriver getDriver() {
-		
+
 		return driver;
 	}
-	
-	public static void setUpDriver() {
-		
+
+	public static void setUpDriver(String browser) {
+
 		if(seleniumDriver ==null) {
-			
-			seleniumDriver = new SeleniumDriver();
+
+			seleniumDriver = new SeleniumDriver(browser);
 		}
 	}
-	
+
 	public static void tearDown() {
-		
+
 		if(driver != null) {
 			driver.close();
 			driver.quit();
--- CucumberAssessmentProject_JUNIT/src/test/java/steps/BeforeActions.java
+++ CucumberAssessmentProject_JUNIT/src/test/java/steps/BeforeActions.java
@@ -1,14 +1,30 @@
 package steps;
 
+//import org.junit.Before;
+
+import cucumber.api.java.Before;
 //import io.cucumber.java.Before;
 import utils.SeleniumDriver;
 
 public class BeforeActions {
 	
-	@cucumber.api.java.Before
-	public static void setUp() {
+	@Before
+	public void setUp() {
 		
-		SeleniumDriver.setUpDriver();
+		String browserName = getParameter("browser");
+		SeleniumDriver.setUpDriver(browserName);
 	}
+	
+	public String getParameter(String name) { 
+		
+		  String browserValue = System.getProperty(name);
+		  if (browserValue == null)
+		     throw new RuntimeException(name + " is not a parameter!");
+
+		  if (browserValue.isEmpty())
+		    throw new RuntimeException(name + " is empty!");
+
+		  return browserValue;
+		 }
 
 }
